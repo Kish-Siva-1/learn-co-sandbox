a7160057b61c07eb181a14340af7d8d202f59c5f
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-17 22:30:34 +0000

    add new portfolio

--- portfolio_manager/app/controllers/portfolio_controller.rb
+++ portfolio_manager/app/controllers/portfolio_controller.rb
@@ -1,3 +1,15 @@
 class PortfolioController < ApplicationController
   
+  get '/portfolio' do 
+    if logged_in?
+      erb :"/portfolio/index"
+    else 
+      erb :"/login"
+    end 
+  end 
+  
+  get '/new' do 
+    
+  end
+  
 end 
