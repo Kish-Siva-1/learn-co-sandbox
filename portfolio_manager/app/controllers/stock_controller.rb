b6ffe28ee7d8a6cc3b32a980d83411142df38e46
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-19 12:53:22 +0000

    will add stock input form

--- portfolio_manager/app/controllers/stock_controller.rb
+++ portfolio_manager/app/controllers/stock_controller.rb
@@ -1,7 +1,12 @@
 class StockController < ApplicationController
   
   get '/stock/new' do 
-    
+    if logged_in?
+      @stocks = Stock.all
+      erb :"stock/new"
+    else 
+      redirect to '/login'
+    end 
   end
   
   post '/stock/new' do 
