d43260198737fe046d29f37822967302efb14a07
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-19 17:28:58 +0000

    adds portfolio and weights

--- portfolio_manager/app/controllers/stock_controller.rb
+++ portfolio_manager/app/controllers/stock_controller.rb
@@ -1,16 +1,36 @@
 class StockController < ApplicationController
   
-  get '/stock/new' do 
+  get '/stock/new/:id' do
     if logged_in?
       @stocks = Stock.all
+      @id = params[:id]
       erb :"stock/new"
     else 
       redirect to '/login'
     end 
   end
   
-  post '/stock/new' do 
+  post '/stock/new/:id' do 
+    store = {}
+    n = 0
+    params[:stock].each do |x,y|
+      break if x == "id"
+      store[x] = y.reject{|_,v| v.blank?}
+    end 
     
+    binding.pry
+    
+    @id = params[:id]
+    binding.pry
+    
+    if logged_in?  
+        portfolio = current_user.portfolios(@id).create(store[:stock])
+        Stock.create(store[:stock]).weights.create(store[:weight])
+        portfolio.stocks << Stock.all.last
+        erb :"/portfolio/index"
+    else 
+      redirect to '/login'
+    end
   end
   
 end 
