3330e36ca1652a36459e1a7596f63e6057ebebdc
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-19 00:15:37 +0000

    will remove validation rules for portfolio weights

--- portfolio_manager/app/models/portfolio.rb
+++ portfolio_manager/app/models/portfolio.rb
@@ -2,8 +2,8 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user 
   has_many :investments
   has_many :stocks, :through => :investments
+  has_many :weights, :through => :investments
 
   validates :name, presence: true
-  validates :stock_weight, presence: true
   
 end 
